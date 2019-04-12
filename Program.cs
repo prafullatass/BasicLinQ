1d77c67a24b5fa16583785ceb4e11477b752521e
Prafullata Sonawane <prafullatass@gmail.com>
2019-04-12 13:13:14 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace basicLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             /*
+            Start with a collection that is of type IEnumerable, which
+            List is and initialize it with some values. This is the
+            class sizes for a selection of NSS cohorts.
+        */
+        List<int> cohortStudentCount = new List<int>()
+        {
+            25, 12, 28, 22, 11, 25, 27, 24, 19
+        };
+
+        /*
+            Now we need to determine which cohorts fell within the range
+            of acceptable size - between 20 and 26 students. Also, sort
+            the new enumerable collection in ascending order.
+        */
+        IEnumerable<int> idealSizes = from count in cohortStudentCount
+            where count < 27 && count > 19
+            orderby count ascending
+            select count;
+
+        // Display each number that was the acceptable size
+        foreach (int c in idealSizes)
+        {
+            Console.WriteLine($"{c}");
+        }
+        }
+    }
+}
