148da3ddbba31b4697681bf68eea5a62e81669a1
Prafullata Sonawane <prafullatass@gmail.com>
2019-04-12 13:18:53 -0500

    added some aggregate methods

--- Program.cs
+++ Program.cs
@@ -2,37 +2,51 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace basicLinq
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-             /*
+namespace basicLinq {
+    class Program {
+        static void Main (string[] args) {
+            /*
             Start with a collection that is of type IEnumerable, which
             List is and initialize it with some values. This is the
             class sizes for a selection of NSS cohorts.
         */
-        List<int> cohortStudentCount = new List<int>()
-        {
-            25, 12, 28, 22, 11, 25, 27, 24, 19
-        };
+            List<int> cohortStudentCount = new List<int> () {
+                25,
+                12,
+                28,
+                22,
+                11,
+                25,
+                27,
+                24,
+                19
+            };
 
-        /*
-            Now we need to determine which cohorts fell within the range
-            of acceptable size - between 20 and 26 students. Also, sort
-            the new enumerable collection in ascending order.
-        */
-        IEnumerable<int> idealSizes = from count in cohortStudentCount
+            /*
+                Now we need to determine which cohorts fell within the range
+                of acceptable size - between 20 and 26 students. Also, sort
+                the new enumerable collection in ascending order.
+            */
+            IEnumerable<int> idealSizes = from count in cohortStudentCount
             where count < 27 && count > 19
             orderby count ascending
             select count;
 
-        // Display each number that was the acceptable size
-        foreach (int c in idealSizes)
-        {
-            Console.WriteLine($"{c}");
-        }
+            // Display each number that was the acceptable size
+            foreach (int c in idealSizes) {
+                Console.WriteLine ($"{c}");
+            }
+
+            Console.WriteLine ($"Largest cohort was {cohortStudentCount.Max()}");
+            Console.WriteLine ($"Smallest cohort was {cohortStudentCount.Min()}");
+            Console.WriteLine ($"Total students is {cohortStudentCount.Sum()}");
+
+            Console.WriteLine ($"Average ideal size is {idealSizes.Average()}");
+
+            // The @ symbol lets you create multi-line strings in C#
+            Console.WriteLine ($@"
+There were {idealSizes.Count()} ideally sized cohorts
+There have been {cohortStudentCount.Count()} total cohorts");
         }
     }
 }
